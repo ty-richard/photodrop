5187cab22d8a9adecdabe7dc4ec97499588b17d5
Ty Richard <tyrichard2@icloud.com>
2018-06-29 09:23:43 -0700

    updates header

---
+++ photo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+
+</script>
---
+++ photo/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Image Upload
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header'
+}
+
+</script>
+
+<style scoped>
+</style>
